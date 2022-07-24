44389625b49434420bd82c79322f6b6d8ae30510
Dlanor91 <limaronald926@gmail.com>
2022-07-24 16:12:06 -0300

    disenno

--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
+import Form from 'react-bootstrap/Form';
+import "./Registro.css";
 
 const Registro = () => {
   return (
-  
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label >Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email"/>
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>      
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <Container className='align-content-center'>      
+      <Form className='formulario '> 
+      <h2 className='formulario-titulo py-2'>Registro</h2>
+      <Row className = "justify-content-center mb-3">    
+        <label htmlFor='user'>Usuario</label>
+        <input type="text" id='user' className="w-50 form-control" placeholder="Usuario ..."/> 
+      </Row>
+      <Row className = "justify-content-center mb-3">
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" id="password" className="w-50 form-control" placeholder="Contraseña ..." /> 
+      </Row>
+      <Row className = "justify-content-center mb-3">
+        <Form.Select className="w-50"> 
+          <option>Seleccione un Departamento</option>
+        </Form.Select>
+      </Row>
+      <Row className = "justify-content-center mb-3">  
+        <Form.Select className="w-50">
+          <option>Seleccione una Ciudad</option>
+        </Form.Select >
+      </Row> 
+      <Row className = "justify-content-center mb-3">
+        <input type="button" className="w-50 btn btn-dark btn-block" value="Registrarse"/>
+      </Row>
+      </Form>
+    </Container>
+ 
    
   )
 }
