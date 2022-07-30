329e9922fccdc0643234e189b3eb4591abb603a4
Lucho2186 <l.lopezperdomo.e@gmail.com>
2022-07-30 14:53:53 -0300

    Listo

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,90 +1,154 @@
-import React, { useState } from "react";
+import React from "react";
 import Titulo from "./titulo/Titulo";
-import Label from "./label/Label";
-import Input from "./input/Input";
 import "./Login.css";
+import { apiLogin } from "../../services/serviciosApi";
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+import TextField from '@mui/material/TextField'
+
+
 
 const Login = () => {
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleChange = (name, value) => {
-    name === "usuario" ? setUser(value) : setPassword(value);
-  };
+    const validationSchema = yup.object({
+        usuario: yup
+            .string('Ingrese su usuario')
+            .required('El usuario no puede estar vacio'),
+        password: yup
+            .string('Ingrese su contraseña')
+            .required('La contraseña no puede estar vacia'),
+    });
 
-  const handleSubmit = () => {
-    let account = { user, password };
+    const formik = useFormik({
+        initialValues: {
+            usuario: '',
+            password: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => Logueo(values)
+    });
 
-    if (account) {
-      console.log("account:", account);
+    const Logueo = async (usuario) => {
+
+        try {
+            console.log('usuario', usuario);
+            const respuesta = await apiLogin(usuario);
+            console.log('respuesta', respuesta);
+            localStorage.setItem("usuario", JSON.stringify(usuario));
+            sessionStorage.setItem("DatosLog", JSON.stringify(respuesta));
+            const dataLog = JSON.parse(sessionStorage.getItem("DatosLog"));
+            alert(dataLog.apiKey);
+        } catch (error) {
+            alert(error);
+        }
     }
-  };
 
-  console.log("usuario:", user);
-  console.log("Password:", password);
+    return (
+        <div className="container">
+            <div className="d-flex justify-content-center h-100">
+                <div className="card align-items-center">
+                    <div className="card-header">
+                        <Titulo text="Bienvenido" />
+                    </div>
 
-  return (
-    <div className="container">
-      <div className="d-flex justify-content-center h-100">
-        <div className="card align-items-center">
-          <div className="card-header">
-            <Titulo text="Bienvenido" />
-          </div>
+                    <div className="card-body">
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="form-group m-3">
+                                <TextField
+                                    fullWidth
+                                    sx={{
+                                        input: {
+                                            color: "white",
+                                            backgroundColor: "grey",
 
-          <div className="card-body">
-            <form>
-              <div className="form-group">
-                <Label text="Usuario" />
-                <Input
-                  attribute={{
-                    id: "usuario",
-                    name: "usuario",
-                    type: "text",
-                    placeholder: "Ingrese su Usuario",
-                  }}
-                  handleChange={handleChange}
-                />
-              </div>
+                                        },
+                                        label: {
+                                            color: "white",
+                                            borderColor: "white",
+                                        },
+                                        fieldset: {
+                                            color: "white"
+                                        },
+                                        "& .MuiOutlinedInput-root": {
+                                            "& fieldset": {
+                                                borderColor: "white"
+                                            },
+                                            "&.Mui-focused fieldset": {
+                                                borderColor: "yellow"
+                                            }
+                                        },
+                                        "& label.Mui-focused": {
+                                            color: "yellow"
+                                        }
+                                    }}
+                                    id="usuario"
+                                    name="usuario"
+                                    label="Usuario"
+                                    value={formik.values.usuario}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.usuario && Boolean(formik.errors.usuario)}
+                                    helperText={formik.touched.usuario && formik.errors.usuario}
+                                />
+                            </div>
 
-              <div className="form-group">
-                <Label text="Contraseña" />
-                <Input
-                  attribute={{
-                    id: "contraseña",
-                    name: "contraseña",
-                    type: "password",
-                    placeholder: "Ingrese su Contraseña",
-                  }}
-                  handleChange={handleChange}
-                />
-              </div>
+                            <div className="form-group m-3">
+                                <TextField
+                                    fullWidth
+                                    sx={{
+                                        input: {
+                                            color: "white",
+                                            backgroundColor: "grey"
+                                        },
+                                        Label: {
+                                            color: "white"
+                                        },
+                                        "& .MuiOutlinedInput-root": {
+                                            "& fieldset": {
+                                                borderColor: "white"
+                                            }, "&.Mui-focused fieldset": {
+                                                borderColor: "yellow"
+                                            }
+                                        },
+                                        "& label.Mui-focused": {
+                                            color: "yellow"
+                                        }
+                                    }}
+                                    id="password"
+                                    name="password"
+                                    label="Password"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.password && Boolean(formik.errors.password)}
+                                    helperText={formik.touched.password && formik.errors.password}
+                                />
 
-              <div className="form-group mt-3">
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className="btn btn-dark btn-block mb-4"
-                >
-                  Ingresar{" "}
-                </button>
-              </div>
-            </form>
-          </div>
+                            </div>
 
-          <div className="card-footer">
-            <div className="text-center">
-              <p className="mt-2">
-                No tienes cuenta?{" "}
-                <a id="registro" href="#!">
-                  Create una!
-                </a>
-              </p>
+                            <div className="form-group mt-3">
+                                <button
+                                    type="submit"
+                                    className="btn btn-dark btn-block m-3"
+                                >
+                                    Ingresar{" "}
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+
+                    <div className="card-footer ">
+                        <div className="text-center">
+                            <span>No tienes cuenta?{" "}
+                                <a id="registro" href="#!">
+                                    Create una!
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
@@ -9,105 +9,105 @@ import Input from './input/Input';
 
 const Registro = () => {
 
-  const [datosUsuario, setDatosUsuario] = useState({
-    user: '',
-    password: ''
-})
- 
-const handleChange = (event) => {
-  /* console.log(event.target.name)
-  console.log(event.target.value) */
-  setDatosUsuario({
-      ...datosUsuario,
-      [event.target.name] : event.target.value
-  })
-  /* console.log(datosUsuario); */
-}  
-
-  const [departamentos, setDepartamentos] = useState([]);
-  const [ciudades, setCiudades] = useState([]);  
-  
-  const [idDepartamento, setIdDepartamento] = useState(0);
-  const [idCiudad, setIdCiudad] = useState(0);
-
-  const capturarIdDepartamento =(event)=>{    
-    setIdDepartamento(event.currentTarget.value)
-    setIdCiudad(0);
-  }
-
-  const capturarIdCiudad =(event)=>{
-    /* console.log(event.target.value); */    
-    setIdCiudad(event.target.value);    
-  }
-
-  const btnRegistro = () =>{
-    const datos = {usuario:`${datosUsuario.user}`, password:`${datosUsuario.password}`, departamento :`${idDepartamento}`, ciudad: `${idCiudad}`}
-    console.log(datos);
-  }
-  useEffect(() => {
-    fetch("https://crypto.develotion.com/departamentos.php")
-  .then(r => r.json())
-  .then(datos => {
-    //console.log(datos);    
-    setDepartamentos(datos.departamentos);        
-  })
-  }, [departamentos])
-  const selectDepartamentos =[{id:0,nombre:"Seleccione un Departamento..."}, ...departamentos];
- 
-  
-  useEffect(() => {    
-    fetch(`https://crypto.develotion.com/ciudades.php?idDepartamento=${idDepartamento}`)
-    .then(r => r.json())
-    .then(datos => {                
-      setCiudades(datos.ciudades);      
+    const [datosUsuario, setDatosUsuario] = useState({
+        user: '',
+        password: ''
     })
-   }, [ciudades])  
-  const selectCiudades =[{id:0,nombre:"Seleccione una Ciudad..."}, ...ciudades];
- 
-  return (
-    <Container className='align-content-center'>      
-      <Form className='formulario '> 
-        <h2 className='formulario-titulo py-2'>Registro</h2>
-        <Row className = "justify-content-center mb-3">    
-          <Label texto="Usuario" htmlEnlace="user"/>
-          <Input attribute={{
-                    id: "user",
-                    name: "user",
-                    type: "text",
-                    placeholder: "Usuario...",
-                  }}
-                  handleChange={handleChange}
-          />                 
-        </Row>
-        <Row className = "justify-content-center mb-3">
-          <Label texto="Contraseña" htmlEnlace="password"/>
-          <Input attribute={{
-                    id: "password",
-                    name: "password",
-                    type: "password",
-                    placeholder: "Contraseña...",
-                  }}
-                  handleChange={handleChange}
-          />  
-        </Row>
-        <Row className = "justify-content-center mb-3">
-          <Form.Select className="w-50 m-2" onChange={capturarIdDepartamento}>
-           {selectDepartamentos.map(dep => <option key={dep.id} value={dep.id}>{dep.nombre}</option>)}
-          </Form.Select>
-        </Row>
-        <Row className = "justify-content-center mb-3" >
-          <Form.Select className="w-50 m-2" onChange={capturarIdCiudad}>
-          {(idDepartamento !=0)? 
-                                selectCiudades.map(city=> <option key={city.id} value={city.id}>{city.nombre}</option>)                                         
-                                : <option>Seleccione un Departamento ...</option>} 
-          </Form.Select>          
-        </Row>         
-        <Row className = "justify-content-center mb-3">
-          <input type="button" className="w-50 btn btn-dark btn-block" onClick={btnRegistro} value="Registrarse"/>
-        </Row>
-      </Form>
-    </Container>
-  )
+
+    const handleChange = (event) => {
+        /* console.log(event.target.name)
+        console.log(event.target.value) */
+        setDatosUsuario({
+            ...datosUsuario,
+            [event.target.name]: event.target.value
+        })
+        /* console.log(datosUsuario); */
+    }
+
+    const [departamentos, setDepartamentos] = useState([]);
+    const [ciudades, setCiudades] = useState([]);
+
+    const [idDepartamento, setIdDepartamento] = useState(0);
+    const [idCiudad, setIdCiudad] = useState(0);
+
+    const capturarIdDepartamento = (event) => {
+        setIdDepartamento(event.currentTarget.value)
+        setIdCiudad(0);
+    }
+
+    const capturarIdCiudad = (event) => {
+        /* console.log(event.target.value); */
+        setIdCiudad(event.target.value);
+    }
+
+    const btnRegistro = () => {
+        const datos = { usuario: `${datosUsuario.user}`, password: `${datosUsuario.password}`, departamento: `${idDepartamento}`, ciudad: `${idCiudad}` }
+        console.log(datos);
+    }
+    useEffect(() => {
+        fetch("https://crypto.develotion.com/departamentos.php")
+            .then(r => r.json())
+            .then(datos => {
+                //console.log(datos);    
+                setDepartamentos(datos.departamentos);
+            })
+    }, [departamentos])
+    const selectDepartamentos = [{ id: 0, nombre: "Seleccione un Departamento..." }, ...departamentos];
+
+
+    useEffect(() => {
+        fetch(`https://crypto.develotion.com/ciudades.php?idDepartamento=${idDepartamento}`)
+            .then(r => r.json())
+            .then(datos => {
+                setCiudades(datos.ciudades);
+            })
+    }, [ciudades])
+    const selectCiudades = [{ id: 0, nombre: "Seleccione una Ciudad..." }, ...ciudades];
+
+    return (
+        <Container className='align-content-center'>
+            <Form className='formulario '>
+                <h2 className='formulario-titulo py-2'>Registro</h2>
+                <Row className="justify-content-center mb-3">
+                    <Label texto="Usuario" htmlEnlace="user" />
+                    <Input attribute={{
+                        id: "user",
+                        name: "user",
+                        type: "text",
+                        placeholder: "Usuario...",
+                    }}
+                        handleChange={handleChange}
+                    />
+                </Row>
+                <Row className="justify-content-center mb-3">
+                    <Label texto="Contraseña" htmlEnlace="password" />
+                    <Input attribute={{
+                        id: "password",
+                        name: "password",
+                        type: "password",
+                        placeholder: "Contraseña...",
+                    }}
+                        handleChange={handleChange}
+                    />
+                </Row>
+                <Row className="justify-content-center mb-3">
+                    <Form.Select className="w-50 m-2" onChange={capturarIdDepartamento}>
+                        {selectDepartamentos.map(dep => <option key={dep.id} value={dep.id}>{dep.nombre}</option>)}
+                    </Form.Select>
+                </Row>
+                <Row className="justify-content-center mb-3" >
+                    <Form.Select className="w-50 m-2" onChange={capturarIdCiudad}>
+                        {(idDepartamento != 0) ?
+                            selectCiudades.map(city => <option key={city.id} value={city.id}>{city.nombre}</option>)
+                            : <option>Seleccione un Departamento ...</option>}
+                    </Form.Select>
+                </Row>
+                <Row className="justify-content-center mb-3">
+                    <input type="button" className="w-50 btn btn-dark btn-block" onClick={btnRegistro} value="Registrarse" />
+                </Row>
+            </Form>
+        </Container>
+    )
 }
 
 export default Registro
