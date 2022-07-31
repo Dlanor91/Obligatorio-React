13ac4d1da77ce3a9a9a0ce8cf2dd308d85125b25
Lucho2186 <l.lopezperdomo.e@gmail.com>
2022-07-31 20:36:43 -0300

    Listo

--- src/services/ServiciosApi.jsx
+++ src/services/ServiciosApi.jsx
@@ -1,103 +1,117 @@
+import { useDispatch } from 'react-redux'
+import { crearTransaccion } from '../features/transacciones/TransaccionesSlice'
+
+
 let apiURL = 'https://crypto.develotion.com';
 
-export const apiLogin = async ({usuario,password}) => {
-    console.log(usuario,password, "LOGIN API");
+export const apiLogin = async ({ usuario, password }) => {
+    console.log(usuario, password, "LOGIN API");
 
-    let headers= new Headers();
-    headers.append("Content-Type","application/json");
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-    let raw= JSON.stringify({
+    let raw = JSON.stringify({
         usuario: usuario,
         password: password,
     });
 
-    let requestOptions= {
+    let requestOptions = {
         method: "POST",
         headers: headers,
         body: raw,
     };
 
-    return fetch (`${apiURL}/login.php`, requestOptions)
-    .then( (response) => response.json() )
-    .then( (result) => {
+    return fetch(`${apiURL}/login.php`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
 
-        switch (result.codigo) {
-            case 200:
-                
-                return Promise.resolve(result);
-                        
-            default:
-                return Promise.reject(result);                
-        }        
-    })
-    .catch ( (error) => Promise.reject (error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde") );
+            switch (result.codigo) {
+                case 200:
+
+                    return Promise.resolve(result);
+
+                default:
+                    return Promise.reject(result);
+            }
+        })
+        .catch((error) => Promise.reject(error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde"));
 };
 
-export const apiRegistro = async ({usuario,password,idDepartamento,idCiudad}) => {
-    console.log(usuario,password,idDepartamento,idCiudad, "REGISTRO API");
+export const apiRegistro = async ({ usuario, password, idDepartamento, idCiudad }) => {
+    console.log(usuario, password, idDepartamento, idCiudad, "REGISTRO API");
 
-    let headers= new Headers();
-    headers.append("Content-Type","application/json");
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-    let raw= JSON.stringify({
+    let raw = JSON.stringify({
         usuario: usuario,
         password: password,
         idDepartamento,
-        idCiudad,
+        idCiudad
     });
 
-    let requestOptions= {
+    let requestOptions = {
         method: "POST",
         headers: headers,
         body: raw,
     };
 
-    return fetch (`${apiURL}/usuarios.php`, requestOptions)
-    .then( (response) => response.json() )
-    .then( (result) => {
+    return fetch(`${apiURL}/usuarios.php`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
 
-        switch (result.codigo) {
-            case 200:
-                return Promise.resolve(result);
-                        
-            default:
-                return Promise.resolve(result);                
-        }        
-    })
-    .catch ( (error) => Promise.reject (error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde") );
+            switch (result.codigo) {
+                case 200:
+                    return Promise.resolve(result);
+
+                default:
+                    return Promise.resolve(result);
+            }
+        })
+        .catch((error) => Promise.reject(error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde"));
 }
 
-export const apiTransaccion = async ({idUsuario,tipoDeOperacion,moneda,cantidad,valorActual}) => {
-    console.log(idUsuario,tipoDeOperacion,moneda,cantidad,valorActual, "TRANSACCION API");
+export const ApiTransaccion = async ({ idUsuario, tipoDeOperacion, moneda, cantidad, valorActual }) => {
+    console.log(idUsuario, tipoDeOperacion, moneda, cantidad, valorActual, "TRANSACCION API");
 
-    let headers= new Headers();
-    headers.append("Content-Type","application/json");
+    const dataLog = JSON.parse(sessionStorage.getItem("DatosLog"));
+    const dispatch = useDispatch();
 
-    let raw= JSON.stringify({
+    let headers = {
+        "Content-Type": "application/json"
+    }
+
+    if (dataLog != null) {
+        headers["apikey"] = `${dataLog.apiKey}`
+    }
+
+    let raw = JSON.stringify({
         idUsuario: idUsuario,
         tipoOperacion: tipoDeOperacion,
-        moneda:moneda,
-        cantidad:cantidad,
-        valorActual:valorActual,
+        moneda: moneda,
+        cantidad: cantidad,
+        valorActual: valorActual,
     });
 
-    let requestOptions= {
+    let requestOptions = {
         method: "POST",
         headers: headers,
         body: raw,
     };
 
-    return fetch (`${apiURL}/transacciones.php`, requestOptions)
-    .then( (response) => response.json() )
-    .then( (result) => {
+    return fetch(`${apiURL}/transacciones.php`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
 
-        switch (result.codigo) {
-            case 200:
-                return Promise.resolve(result);
-                        
-            default:
-                return Promise.resolve(result);                
-        }        
-    })
-    .catch ( (error) => Promise.reject (error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde") );
+            switch (result.codigo) {
+                case 200:
+                    raw.id = result.id;
+                    dispatch(crearTransaccion(raw))
+                    return Promise.resolve(result);
+
+                default:
+                    return Promise.resolve(result);
+            }
+        })
+        .catch((error) => Promise.reject(error.mensaje ? error.mensaje : "Ah ocurrido un error, vuelva a intentarlo mas tarde"));
 }
--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -2,8 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import transaccionesReducer from '../features/transacciones/TransaccionesSlice'
 import monedasReducer from '../features/monedas/MonedasSlice'
-import DepartamentosSlice,{departamentosSlice} from "../features/departamentos/DepartamentosSlice";
-import CiudadesSlice,{ciudadesSlice} from "../features/ciudades/CiudadesSlice";
+import DepartamentosSlice from "../features/departamentos/DepartamentosSlice";
+import CiudadesSlice from "../features/ciudades/CiudadesSlice";
 
 export const store = configureStore({
 
@@ -11,7 +11,7 @@ export const store = configureStore({
         transacciones: transaccionesReducer,
         monedas: monedasReducer,
         departamentos: DepartamentosSlice,
-        ciudades: CiudadesSlice 
+        ciudades: CiudadesSlice
     },
 })
 
