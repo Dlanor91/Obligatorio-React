ebc45c489310020af0bf10d1eaf7e1de2a981187
Dlanor91 <limaronald926@gmail.com>
2022-07-31 14:04:03 -0300

    dispa

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import DepartamentosSlice,{departamentosSlice} from "../features/DepartamentosSlice";
+
 
 export const store = configureStore({
     reducer:{
-        
+      departamentos: DepartamentosSlice 
     }
-})
+});
