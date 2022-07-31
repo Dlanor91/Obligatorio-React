16ce68d453c0b2babda38b3669d42ad193ad5eb9
Lucho2186 <l.lopezperdomo.e@gmail.com>
2022-07-31 14:09:14 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "./App.css";
 
 import Login from "./components/login/Login";
 import Registro from "./components/registro/Registro";
+import {store} from './store/Store'
+import {Provider} from 'react-redux'
+import Transacciones from './components/dashboard/transacciones/Transacciones'
+import Dashboard  from "./components/dashboard/Dashboard";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Registro />
+      
+        <Provider store ={store} >
+      
+        <Login />
+        {/* <Registro />   */} 
+        <Dashboard/>
+
+      </Provider>
     </div>
   );
 }
