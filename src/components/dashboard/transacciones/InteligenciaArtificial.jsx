0c4bafcf335dc6370daebb59711e4f7829d7a7ae
Lucho2186 <l.lopezperdomo.e@gmail.com>
2022-08-06 20:38:20 -0300

    ok

--- src/components/dashboard/transacciones/InteligenciaArtificial.jsx
+++ src/components/dashboard/transacciones/InteligenciaArtificial.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 
 export const InteligenciaArtificial = () => {
+
+    const monedas = useSelector(state => state.monedas.monedas)
+    const transacciones = useSelector(state => state.transacciones.transacciones);
+    const ultimaTran = transacciones[transacciones.length - 1]
+
+    const buscarTranMoneda = transacciones.filter((t, indice) => t.moneda === 2)
+
+    const mon = monedas.find(m => m.id === ultimaTran.moneda)
+    const idMonedas = monedas.map(m => m.id);
+
     return (
-        <div>InteligenciaArtificial</div>
+
+        <div>{mon.cotizacion > ultimaTran.valor_actual ? "Compra" : "Venta"}
+            <div>InteligenciaArtificial</div>
+        </div>
+
+
+
     )
 }
+
